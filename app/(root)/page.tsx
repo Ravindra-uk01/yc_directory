243aa02ec76ad2aa75387981cfbc9f45e2607d75
Ravindra Singh Rayal <rayalravi2001@gmail.com>
2025-04-10 08:11:41 +0530

    added nextauth

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+   <>
+    <section className="pink_container bg-blue-500">
+      <div className="pink_container_inner">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <p className="text-2xl">This is a test</p>
+      </div>
+    </section>
+    <h1>Home</h1>
+   </>
+  );
+}
