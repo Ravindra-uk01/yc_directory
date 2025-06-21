031071ef3998695ca7f0ac66a001c8aca2fe3b14
Ravindra Singh Rayal <rayalravi2001@gmail.com>
2025-06-21 07:10:18 +0530

    added the user profile page

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,40 +1,55 @@
-import { auth } from '@/auth';
-import { client } from '@/sanity/lib/client';
-import { AUTHOR_BY_ID_QUERY } from '@/sanity/lib/queries';
-import Image from 'next/image';
-import { notFound } from 'next/navigation';
-import React from 'react'
+import { auth } from "@/auth";
+import { StartupCardSkeleton } from "@/components/StartupCard";
+import UserStartups from "@/components/UserStartups";
+import { client } from "@/sanity/lib/client";
+import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import React, { Suspense } from "react";
 
-const UserProfile = async({params}:{params: Promise<{id: string}>}) => {
+export const experimental_ppr = true;
+const UserProfile = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
+  const session = await auth();
 
-    const {id} = await params;
-    const session = await auth();
+  const user = await client.fetch(AUTHOR_BY_ID_QUERY, { id });
+  if (!user) return notFound();
 
-    const user = await client.fetch(AUTHOR_BY_ID_QUERY, {id});
-    if (!user) return notFound();
+  console.log("user is ", user);
 
-    console.log('user is ', user)
-    
-   return (
-    <> 
-        <section className='profile_container' >
-            <div className="profile_card" >
-                <div className="profile_title" >
-                    <h3> {user.name} </h3>
-                </div>
-            </div>
-            <Image
-                src={user.image}
-                alt={user.name}
-                width={100}
-                height={100}
-                className="profile_image" />
+  return (
+    <>
+      <section className="profile_container">
+        <div className="profile_card">
+          <div className="profile_title">
+            <h3 className="text-24-black uppercase text-center line-clamp-1" > {user.name} </h3>
+          </div>
 
-            <p>@{user.username}</p>
-            <p>{user.description}</p>
-        </section>
-     </>
-  )
-}
+          <Image
+            src={user.image}
+            alt={user.name}
+            width={220}
+            height={220}
+            className="profile_image"
+          />
+
+          <p className="text-center mt-1 text-30-extrabold">@{user.username}</p>
+          <p className="text-center mt-1 text-14-normal">{user.bio}</p>
+        </div>
+        <div className="flex-1 flex flex-col gap-5 lg:-mt-5">
+            <p className="text-30-bold">
+                {session?.id === id ? "Your" : "All"} Startups
+            </p>
+            <ul className="card_grid-sm">
+                <Suspense fallback={<StartupCardSkeleton />}>
+                    <UserStartups id={id} />
+                </Suspense>
+            </ul>
+        </div>
+
+      </section>
+    </>
+  );
+};
 
 export default UserProfile;
